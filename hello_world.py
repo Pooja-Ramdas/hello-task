279add1e3a69b1085cc422a0ac424eecf6e3e641
Pooja Ramdas <pooja.ramdas05@gmail.com>
2024-01-12 23:36:21 +0530

    Third and final commit: Further enhanced the script to greet with OS information in addition to the name.

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,10 @@
-#Greeting by name
+#Greeting by name and welcoming into an OS
+import platform
 name = input("What's your name? Type here: ")
-print(f"Hello, {name}.")
+current_system=platform.system()
+if current_system=="Linux":
+    with open("/etc/os-release", "r") as file:
+        os_about = file.readline()
+    print(f"Hello {name}, welcome to {current_system}. Here is the OS information: {os_about}")
+else:
+    print(f"Hello {name}, welcome to {current_system}.")
